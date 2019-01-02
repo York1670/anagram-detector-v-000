3049d058b7dd4de553b3c65756390eeffc7fef1b
Bobbi York <york1670@gmail.com>
2019-01-02 01:04:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,28 +1,15 @@
 class Anagram
-  attr_accessor :word, :some_words
+  attr_accessor :word
+  anagrams = [] 
 
-  anagrams = []
-  same_length = []
-
-  def initialize(word,some_words)
+  def initialize(word)
     @word = word.downcase
-    @some_words = some_words
   end
 
-  def anagram_test
-    @some_words.each do|some_word|
-      if @word.length == some_word.length
-        same_length << some_word
-      else
-        return "Try again"
-      end
-    end
-
-    same_length.each do |some_word|
+  def match(some_words) 
+    some_words.each do |some_word|
       if @word.split("").sort.join("") == some_word.downcase.split("").sort.join("")
         anagrams << some_word
-      else
-        return "try again"
       end
     end
     return anagrams
