a37c5292c5784435dd372d29ce3b84ad9567b30e
Bobbi York <york1670@gmail.com>
2019-01-02 01:04:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
     @word = word.downcase
   end
 
-  def match(some_words) 
+  def match(some_words)
     some_words.each do |some_word|
       if @word.split("").sort.join("") == some_word.downcase.split("").sort.join("")
         anagrams << some_word
